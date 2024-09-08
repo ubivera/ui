6489395adbde4dd708605a35612c03cb6e12db63
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-09-08 13:10:33 -0400

    Updated config to node module resolution

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
-    variant: 'primary' | 'secondary';
+    variant: 'primary' | 'secondary' | 'optional';
 };
 
 export default function Button({ className, variant, ...props }: ButtonProps) {
