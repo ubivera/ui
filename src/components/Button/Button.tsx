80e0f01f601892d2eb23050341f9f482e9206c10
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-09-09 11:51:37 -0400

    Updated Button to use logic companion

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import ButtonLogic from './Button.logic';
 
 type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
-    variant: 'primary' | 'secondary' | 'optional';
+    variant?: 'primary' | 'secondary' | 'optional';
+    onLogicClick?: (event: MouseEvent) => void;
 };
 
-export default function Button({ className, variant, ...props }: ButtonProps) {
-    return <button {...props} className={variant} />;
+export default function Button({ className, variant, onLogicClick, ...props }: ButtonProps) {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    let buttonLogic: ButtonLogic | null = null;
+
+    useEffect(() => {
+        if (buttonRef.current) {
+            buttonLogic = new ButtonLogic(buttonRef.current);
+
+            if (onLogicClick) {
+                buttonLogic.setOnClickHandler(onLogicClick);
+            }
+        }
+
+        return () => {
+            buttonLogic?.destroy();
+        };
+    }, [onLogicClick]);
+
+    return <button ref={buttonRef} {...props} className={variant} />;
 }
