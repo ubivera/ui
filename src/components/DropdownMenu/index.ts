65119595af51447d4937e2bd34b4ca347fc56076
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-09-28 11:13:26 -0400

    Updated export of Dropdown

--- src/components/DropdownMenu/index.ts
+++ src/components/DropdownMenu/index.ts
@@ -1,3 +1,3 @@
 export { default as DropdownMenu } from './DropdownMenu';
 export { default as DropdownTrigger } from './DropdownTrigger';
-export { default as DropdownMenuContent } from './DropdownMenuContext';
+export { default as DropdownMenuContent } from './DropdownMenuContent';
