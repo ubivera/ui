dbca497db26d9a9d6ae19ec0e383820e523b4613
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-10-01 16:37:03 -0400

    Added license notices

--- src/types/scss.d.ts
+++ src/types/scss.d.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ubivera. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
