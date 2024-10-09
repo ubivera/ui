0dce851cdbfb74f0fa7c46d2580819be294df61c
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-10-09 09:15:17 -0400

    Staged sub menus and action items

--- src/components/DropdownMenu/DropdownMenuAction.tsx
+++ src/components/DropdownMenu/DropdownMenuAction.tsx
@@ -3,3 +3,19 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import React from 'react';
+
+interface DropdownMenuActionProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    label: string;
+    onAction: () => void;
+}
+
+const DropdownMenuAction: React.FC<DropdownMenuActionProps> = ({ label, onAction, ...props }) => {
+    return (
+        <button className="dropdown-menu-action" onClick={onAction} {...props}>
+            {label}
+        </button>
+    );
+};
+
+export default DropdownMenuAction;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,11 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import './_globals.scss';
+import './Global.scss';
 export { default as Button } from './Button';
 export { DropdownMenu as DropdownMenu } from './DropdownMenu';
 export { DropdownMenuContent as DropdownMenuContent } from './DropdownMenu';
 export { DropdownTrigger as DropdownTrigger } from './DropdownMenu';
+export { DropdownMenuAction as DropdownMenuAction } from './DropdownMenu';
+export { DropdownMenuSeparator as DropdownMenuSeparator } from './DropdownMenu';
+export { DropdownMenuSubMenu as DropdownMenuSubMenu } from './DropdownMenu';
