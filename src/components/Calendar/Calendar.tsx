b7b74885e4ba26ba0e82f5896a375963ffd41d65
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-10-14 17:30:57 -0400

    Basic calendar component functionality (#22)
    * Basic calendar creation of current date
    
    * Updated style of days

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,10 +3,31 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CalendarHeader from './CalendarHeader';
+import CalendarBody from './CalendarBody';
+import './Calendar.scss';
 
-const Calendar: React.FC = () => {
-    return <div className='calendar'></div>
+interface CalendarProps {
+    date?: Date;
+    showDaysOfWeek?: boolean;
+}
+
+const Calendar: React.FC<CalendarProps> = ({ date, showDaysOfWeek = true }) => {
+    const [currentDate, setCurrentDate] = useState<Date>(date || new Date());
+
+    useEffect(() => {
+        if (date) {
+            setCurrentDate(date);
+        }
+    }, [date]);
+
+    return (
+        <div className='calendar' data-date={currentDate.toISOString()}>
+            {showDaysOfWeek && <CalendarHeader />}
+            <CalendarBody currentDate={currentDate} />
+        </div>
+    );
 };
 
 export default Calendar;
---
+++ src/utils/daysOfWeek.ts
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ubivera. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export const DaysOfWeekShort: string[] = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+export const DaysOfWeekFull: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+export default DaysOfWeekShort;
