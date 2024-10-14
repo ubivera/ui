29454b1225d1b7ddf0153a428be26246141a48ee
Joseph Tindall <77989761+josephtindall@users.noreply.github.com>
2024-10-14 13:38:54 -0400

    Staged components for date picker (#21)
    * Staged style for date picker
    
    * Staged label and calendar

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ubivera. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import React from 'react';
+
+const Calendar: React.FC = () => {
+    return <div className='calendar'></div>
+};
+
+export default Calendar;
