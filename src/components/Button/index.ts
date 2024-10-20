78144ec1a57b89b1a8cfcdcaeb08d7a904aeb496
Joseph <77989761+josephtindall@users.noreply.github.com>
2024-10-20 14:42:54 -0400

    Reset components logic

--- src/components/Button/index.ts
+++
@@ -1,3 +0,0 @@
-export { Label } from './Label';
-export { default } from './Button';
-export { LeftIcon, RightIcon } from './Icon';
--- src/components/DatePicker/index.ts
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Ubivera. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-export { default } from './DatePicker';
